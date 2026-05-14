942702c79fc7f87a41d4115c8893a160d7ffb10a
Im10furry <xiaoliuwj.china@gmail.com>
2026-05-14 14:41:36 +0800

    feat: add protocol constants

---
+++ constant/command.go
@@ -0,0 +1,51 @@
+package constant
+
+const (
+	StartMarker1 = 0x23
+	StartMarker2 = 0x23
+
+	CmdLogin       = 0x01
+	CmdRealtime    = 0x02
+	CmdReissue     = 0x03
+	CmdLogout      = 0x04
+	CmdPlatLogin   = 0x05
+	CmdPlatLogout  = 0x06
+	CmdHeartbeat   = 0x07
+	CmdTimeCalibr  = 0x08
+
+	RespSuccess    = 0xFE
+
+	EncNone   = 0x01
+	EncRSA    = 0x02
+	EncAES128 = 0x03
+	EncAbnormal = 0xFE
+
+	VINLength = 17
+
+	LoginSuccess   = 0x01
+	LoginCarNotFound    = 0x02
+	LoginTerminalNotFound = 0x03
+	LoginResultAbnormal = 0xFE
+
+	HeaderSize = 24
+	MinPacketSize = 25
+	MaxPacketSize = 65535 + 25
+)
+
+var CommandNames = map[byte]string{
+	CmdLogin:      "LOGIN",
+	CmdRealtime:   "REALTIME",
+	CmdReissue:    "REISSUE",
+	CmdLogout:     "LOGOUT",
+	CmdPlatLogin:  "PLAT_LOGIN",
+	CmdPlatLogout: "PLAT_LOGOUT",
+	CmdHeartbeat:  "HEARTBEAT",
+	CmdTimeCalibr: "TIME_CALIBRATION",
+}
+
+var LoginResultNames = map[byte]string{
+	LoginSuccess:    "SUCCESS",
+	LoginCarNotFound:     "CAR_NOT_FOUND",
+	LoginTerminalNotFound: "TERMINAL_NOT_FOUND",
+	LoginResultAbnormal:  "ABNORMAL",
+}
